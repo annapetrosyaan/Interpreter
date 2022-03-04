b8fff822cda7b0fe58ec3a0d11e677e30e97e939
annapetrosyaan <89910608+annapetrosyaan@users.noreply.github.com>
2022-03-04 15:29:54 +0400

    Update Tokenizer.h

--- Tokenizer.h
+++ Tokenizer.h
@@ -1,54 +1,50 @@
-#pragma once
+#ifndef _TOKENIZER_H_
+#define _TOKENIZER_H_
 
-#include <iostream>
 #include <fstream>
 #include <sstream>
 #include <vector>
-
-using namespace std;
+#include <string>
 
 namespace Interpeter
 {
-	namespace Tokenizer
-	{
+  namespace Tokenizer
+  {
+	enum TOKEN_TYPE {
+	TOKEN_WORD,	 // nome
+	TOKEN_KEYWORD,	// if end while
+	TOKEN_METHOD_OPEN,	//	(
+	TOKEN_METHOD_CLOSE,	//	)
+	TOKEN_SCOPE_OPEN,  // {
+	TOKEN_SCOPE_CLOSE, // }
+	TOKEN_ASSIGNMENT, // =
+	TOKEN_TERMINATOR, // \n ;
+	TOKEN_OPERATOR,	 // + - * /
+	TOKEN_STRING	// ' "
+	};
 
-		enum TOKEN_TYPE {
-			TOKEN_WORD,				//	nome
-			TOKEN_KEYWORD,			//	if end while
-			TOKEN_METHOD_OPEN,		//	(
-			TOKEN_METHOD_CLOSE,		//	)
-			TOKEN_SCOPE_OPEN,		//	{
-			TOKEN_SCOPE_CLOSE,		//	}
-			TOKEN_ASSIGNMENT,		//	=
-			TOKEN_TERMINATOR,		//	\n ;
-			TOKEN_OPERATOR,			// + - * /
-			TOKEN_STRING			// ' "
-		};
+class Token
+{
+public:
+	Token(TOKEN_TYPE type, std::string value);
+	std::string print();
+	std::string simulate();
+	TOKEN_TYPE type();
+	std::string val();
+private:
+	TOKEN_TYPE _type;
+	std::string _value;
+};
 
+enum TOKENIZER_STATUS {
+	TE0, //	Status OK
+	TE1, //	Source code not exist
+	TE2, //	Character not accepted
+	};
 
+TOKENIZER_STATUS tokenizer(char* path, std::vector<Token>* tokenList);
+TOKENIZER_STATUS readSource(std::string& source, std::vector<Token>* tokenList);
+  } // namespace Tokenizer
+} // namespace Interpreter
 
-		class Token
-		{
-		public:
-			Token(TOKEN_TYPE type, string value);
-			string print();
-			string simulate();
-			TOKEN_TYPE type();
-			string val();
-		private:
-			TOKEN_TYPE _type;
-			string _value;
-		};
-
-
-		enum TOKENIZER_STATUS {
-			TE0,	//	Status OK
-			TE1,	//	Source code not exist
-			TE2,	//	Character not accepted
-		};
-
-		TOKENIZER_STATUS tokenizer(char* path, vector<Token>* tokenList);
-		TOKENIZER_STATUS readSource(string& source, vector<Token>* tokenList);
-
-	}
-}
+#endif // !_TOKENIZER_H_
